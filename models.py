1f51b505ed631fc119aa78e7d0a7a7c6cb17dffb
Lars Johansen <popkid@zenbe.com>
2008-10-25 12:36:48 +0200

    Made some small estetical changes to the category model.

--- models.py
+++ models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 class Category(models.Model):
     title = models.CharField('title', max_length=300, help_text='No longer than 300 characters.')
-    slug = models.SlugField('slug', unique=True)
+    slug = models.SlugField('slug', unique=True, help_text="Unique 'slug' for use in URL etc.")
     description = models.TextField()
     
     class Meta:
+        ordering = ['title']
         verbose_name =('category')
         verbose_name_plural = ('categories')
     
@@ -14,4 +15,8 @@ class Category(models.Model):
         
     def __unicode__(self):
         return u'%s' % self.title
+        
+    def get_absolute_url(self):
+        return "/categories/%s/" % self.slug
+
 
