58abe66fcbed945b686888b0723ca465d3a9750f
Lars Johansen <popkid@zenbe.com>
2008-10-28 01:10:20 +0100

    Changed to generic views

--- views.py
+++ views.py
@@ -1,8 +1,3 @@
-from django.shortcuts import render_to_response
-from wesseltoft.models import Entry
 
 
-def entries_index(request):
-    return render_to_response('wesseltoft/entry_index.html',
-                                                      {  'entry_list': Entry.objects.all()  })
-                                                      
+
